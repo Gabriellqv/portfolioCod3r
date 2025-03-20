6608f3979ed2e89025b5f4004fb0609963360dec
Gabriellqv <gabriellqv@gmail.com>
2025-03-20 16:33:56 -0300

    feat(backend): api de tecnologias e projetos

---
+++ backend/src/tecnologia/tecnologia.controller.ts
@@ -0,0 +1,19 @@
+import { Tecnologia } from '@core'
+import { Controller, Get } from '@nestjs/common';
+import { TecnologiaPrisma } from './tecnologia.prisma';
+
+@Controller('tecnologias')
+export class TecnologiaController {
+  constructor(private readonly repo: TecnologiaPrisma) { }
+
+  @Get()
+  async obterTecnologias(): Promise<Tecnologia[]> {
+    return this.repo.obterTecnologias();
+  }
+
+
+  @Get('destaques')
+  async obterDestaques(): Promise<Tecnologia[]> {
+    return this.repo.obterDestaques();
+  }
+}

551a7a51e7522ac2c35933ac6c132980d95ed48d
Gabriellqv <gabriellqv@gmail.com>
2025-03-21 13:43:18 -0300

    feat(frontend): versão inicial do projeto

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Portfolio",
+  description: "Portfolio de projetos de desenvolvimento",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${montserrat.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
